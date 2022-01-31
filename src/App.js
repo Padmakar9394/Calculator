82388754db11ff2cc4d7b4156b1f03cfd78e808e
prg2024btech@gmail.com <prg2024btech@gmail.com>
2022-01-31 17:42:32 +0530

    created calculator

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//components
+import Calculator from "./components/calculator";
+
+
+function App() {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
